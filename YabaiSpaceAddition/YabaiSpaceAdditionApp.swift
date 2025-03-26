9348282208ead07d180b3b9def00868565a2bbfa
ctih <onni09nevala@gmail.com>
2025-03-26 16:46:04 +0200

    Initial Commit

---
+++ YabaiSpaceAddition/YabaiSpaceAdditionApp.swift
@@ -0,0 +1,17 @@
+//
+//  YabaiSpaceAdditionApp.swift
+//  YabaiSpaceAddition
+//
+//  Created by Onni Nevala on 26.3.2025.
+//
+
+import SwiftUI
+
+@main
+struct YabaiSpaceAdditionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YabaiSpaceAdditionUITests/YabaiSpaceAdditionUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  YabaiSpaceAdditionUITestsLaunchTests.swift
+//  YabaiSpaceAdditionUITests
+//
+//  Created by Onni Nevala on 26.3.2025.
+//
+
+import XCTest
+
+final class YabaiSpaceAdditionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
